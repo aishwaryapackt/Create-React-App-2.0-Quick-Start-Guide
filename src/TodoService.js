ac9836cc7f6cb374653bf6a2f8e96fd87936b812
Brandon Richey <brandon.richey@greenhouse.io>
2019-02-14 10:12:18 -0500

    Chapter 8 without a syntax error

--- src/TodoService.js
+++ src/TodoService.js
@@ -9,18 +9,6 @@ const fetchTodos = async () => {
 };
 
 const createTodo = async description => {
-<<<<<<< HEAD
-  const res = await fetch("/api/todos", {
-    method: "POST",
-    headers: { accept: "application/json", "content-type": "application/json" },
-    body: JSON.stringify({
-      description: description,
-      critical: false,
-      done: false
-    })
-  });
-  const json = await res.json();
-=======
   const config = {
     headers: { accept: "application/json", "content-type": "application/json" }
   };
@@ -31,24 +19,15 @@ const createTodo = async description => {
   };
   const res = await axios.post("/api/todos", data, config);
   const json = await res.data;
->>>>>>> 5da61c1... Chapter 8 complete
   return { status: res.status, todos: json.todos };
 };
 
 const deleteTodo = async todoId => {
-<<<<<<< HEAD
-  const res = await fetch(`/api/todos/${todoId}`, {
-    method: "DELETE",
-    headers: { accept: "application/json", "content-type": "application/json" }
-  });
-  const json = await res.json();
-=======
   const config = {
     headers: { accept: "application/json", "content-type": "application/json" }
   };
   const res = await axios.delete(`/api/todos/${todoId}`, config);
   const json = await res.data;
->>>>>>> 5da61c1... Chapter 8 complete
   return { status: res.status, todos: json.todos };
 };
 
